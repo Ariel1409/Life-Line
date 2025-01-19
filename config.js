cd922998a0a8abfbbc312d84b3a65db7a0918444
Ariel1409 <garciariel14@gmail.com>
2025-01-19 12:17:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbydxgNfyd4E0mUGjXqc28eLwnUlceFRoVYF0f0cz2Kj3l_O6YmHD0dkj5XVlHisZFjesw/exec";
